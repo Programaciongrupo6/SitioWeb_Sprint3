7d38db4b8027f91670595f0e1f6ee738ad680687
Edwin j <you@example.comedwinarevalo88@gmail.com>
2021-10-09 16:44:35 -0500

    enviar

--- js/registro.js
+++ js/registro.js
@@ -1,35 +1,31 @@
-//let registros = [{"usuario":"Juan","contrasena":"Secure"},{"usuario":"Carlos","contrasena":"SecurePassword"},{"usuario":"pedro","contrasena":"123456789"}]
 let registros = []
 
 function agregarRegistro() {
-
+    // let usuario = document.getElementById("in_usuario").value;
+    // let contrasena = document.getElementById("in_contrasena").value;
+    // registros.push({usuario,contrasena});
     let inputUsuario = document.getElementById("in_usuario").value;
     let inputContrasena = document.getElementById("in_contrasena").value;
     registros.push({ "usuario": inputUsuario, "contrasena": inputContrasena })
     inputUsuario.value = ""
     inputContrasena.value = ""
-    console.log("1-Guardado")
-    console.log("2-" + inputUsuario)
-    console.log("3-" + inputContrasena)
-    console.log("4-" + registros)
-    console.log("5-" + registros[0].contrasena)
-    console.log(registros)
 }
 
 function filtrarPorContrasena(arreglo, filtro) {
-    console.log(arreglo, filtro)
-    let registrosFiltrados = []
-    for (let i = 0; i < arreglo.length; i++) {
-        let nombre = arreglo[i].usuario;
-        let pasword = arreglo[i].contrasena;
-        if (pasword.length <= filtro) {
-            registrosFiltrados.push({ "usuario": nombre, "contrasena": pasword })
-        }
-    }
-    console.log(registrosFiltrados)
-    return registrosFiltrados
+    // let registrosFiltrados = []
+    // for (let i = 0; i < arreglo.length; i++) {
+    //     let nombre = arreglo[i].usuario;
+    //     let pasword = arreglo[i].contrasena;
+    //     if (pasword.length <= filtro) {
+    //         registrosFiltrados.push({ "usuario": nombre, "contrasena": pasword })
+    //     }
+    // }
+    // return registrosFiltrados
+    let formularios = arreglo.filter(formulario => (formulario.contrasena).length <= filtro);
+    console.log(formularios);
+    return formularios;
 }
 
-module.exports.registros = registros;
-module.exports.filtrarPorContrasena = filtrarPorContrasena;
-module.exports.agregarRegistro = agregarRegistro;
+// module.exports.registros = registros;
+// module.exports.filtrarPorContrasena = filtrarPorContrasena;
+// module.exports.agregarRegistro = agregarRegistro;
